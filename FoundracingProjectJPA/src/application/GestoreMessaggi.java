efdeee47bfc5267df611269d3243eea1169594de
guidogagl <guidogagl@users.noreply.github.com>
2019-10-22 16:58:55 +0200

    set task1

---
+++ FoundracingProjectJPA/src/application/GestoreMessaggi.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GestoreMessaggi {
+	private DepositoDati d;
+	private TableMessage tm;
+	private String agencyName;
+	private Timer timer = null;
+	private TimerTask task = null;
+	public GestoreMessaggi( DepositoDati d,TableMessage tm,String agencyName)
+	{
+		this.d=d;
+		this.tm=tm;
+		this.agencyName=agencyName;
+		
+	}
+	public void startAggiornamentoTabella() {
+		//while(true) {
+		
+			timer = new Timer();
+			task = new TimerTask() {
+				public void run() {
+					
+					aggiornaTabellaMessaggi();
+					
+					/*ThreadGestioneMessaggi tgm=new ThreadGestioneMessaggi(d,tm,agencyName);
+					tgm.start();
+					try {
+						tgm.join();
+					}catch(Exception e) {
+						e.printStackTrace();
+					}*/
+				}
+			};
+			timer.schedule(task, 0, 5000);
+			
+			/*try {
+				tgm.join(2000);
+			}catch(Exception e) {e.printStackTrace();}*/
+		//}
+							
+	}
+	
+	private void aggiornaTabellaMessaggi() {
+		List<RowTableMessage> messaggiDaAggiungere = d.getMessages(agencyName);
+		tm.updateMessages(messaggiDaAggiungere);
+	}
+	
+	
+	public void endAggiornamentoTabella() {
+		timer.cancel();
+		timer.purge();
+	}
+}
---
+++ FoundracingProjectJPA/src/application/RowTableMessage.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.awt.Component;
+
+import javafx.beans.property.*;
+import javafx.scene.image.*;
+
+public class RowTableMessage extends Component{
+	
+	private SimpleIntegerProperty id; //id messaggio
+	private SimpleIntegerProperty id_project;
+	private SimpleStringProperty data;
+	private SimpleStringProperty mittente;
+	private SimpleStringProperty destinatario;
+	private SimpleStringProperty messaggio;
+	private SimpleIntegerProperty stake;
+	
+	public RowTableMessage(int id, int id_project, String data, String mittente, String destinatario, String messaggio,
+			int stake) {
+		
+		this.id = new SimpleIntegerProperty(id);
+		this.id_project = new SimpleIntegerProperty(id_project);
+		this.data = new SimpleStringProperty(data);
+		this.mittente = new SimpleStringProperty(mittente);
+		this.destinatario = new SimpleStringProperty(destinatario);
+		this.messaggio = new SimpleStringProperty(messaggio);
+		this.stake = new SimpleIntegerProperty(stake);
+	}
+	
+	
+	public int getId() {return id.get();}
+	public int getId_project() {return id_project.get();}
+	public String getData() {return data.get();}
+	public String getMittente() {return mittente.get();}
+	public String getDestinatario() {return destinatario.get();}
+	public String getMessaggio() {return messaggio.get();}
+	public int getStake() {return stake.get();}
+
+}
